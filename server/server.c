d13570edebbe7f9133a32defeaacbf57954d5da1
Hussien Mostafa <36410801+HMS-ELKHOLY@users.noreply.github.com>
2022-11-17 14:59:40 +0200

    update server code

--- server/server.c
+++ server/server.c
@@ -3,6 +3,7 @@
 ST_accountsDB_t accountsDB[255];
 uint8_t currentIndexAccDb = -1;
 ST_transaction_t transactionDB[255] = { 0 };
+uint8_t trnscDbIndex;
 EN_transState_t recieveTransactionData(ST_transaction_t* transData) {
 	uint8_t foundAccount = 0;
 	for (int i = 0; i < 255; i++) {
@@ -53,7 +54,33 @@ EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t*
 	return SERVER_OK;
 }
 EN_serverError_t saveTransaction(ST_transaction_t* transData){
+
+
+	transactionDB[trnscDbIndex].cardHolderData = transData->cardHolderData;
+	transactionDB[trnscDbIndex].terminalData = transData->terminalData;
+	transactionDB[trnscDbIndex].transactionSequenceNumber +=1;
+	trnscDbIndex++;
+
+	return SERVER_OK;
 }
 void listSavedTransactions(void) {
+	uint8_t* transStateVal[] = { "APPROVED", "DECLINED_INSUFFECIENT_FUND", "DECLINED_STOLEN_CARD", "FRAUD_CARD", "INTERNAL_SERVER_ERROR" };
+	for (int i = 0; i < trnscDbIndex; i++)
+	{
+		printf("#########################");
+		printf("Transaction Sequence Number: %d", transactionDB[trnscDbIndex].transactionSequenceNumber );
+		printf("Transaction Date:%s", transactionDB[trnscDbIndex].terminalData.transactionDate);
+		printf("Transaction Amount:%f", transactionDB[trnscDbIndex].terminalData.transAmount);
+		printf("Transaction State:%s",transStateVal[ transactionDB[trnscDbIndex].transState]);
+		printf("Transaction Max Amount::%f", transactionDB[trnscDbIndex].terminalData.maxTransAmount);
+		printf("Cardholder Name:%s", transactionDB[trnscDbIndex].cardHolderData.cardHolderName);
+		printf("PAN:%s", transactionDB[trnscDbIndex].cardHolderData.primaryAccountNumber);
+		printf("Card Expiration Date: %s", transactionDB[trnscDbIndex].cardHolderData.cardExpirationDate);
+		printf("#########################");
+
+
+
+	}
 
 }
+
